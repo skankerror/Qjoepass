0d9fd2c0c75c48a9396f2be1db2f43af41d86333
skankerror <michael.creusy@yahoo.fr>
2020-05-28 18:08:05 -0300

    joepass style juggler

---
+++ animationfactory.cpp
@@ -0,0 +1,6 @@
+#include "animationfactory.h"
+
+AnimationFactory::AnimationFactory()
+{
+
+}
---
+++ animationfactory.h
@@ -0,0 +1,13 @@
+#ifndef ANIMATIONFACTORY_H
+#define ANIMATIONFACTORY_H
+
+#include <QObject>
+
+class AnimationFactory
+{
+  Q_OBJECT
+public:
+  AnimationFactory();
+};
+
+#endif // ANIMATIONFACTORY_H
